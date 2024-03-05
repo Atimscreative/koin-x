2e2148ff024c12f1a07caed5c53b7863ed957ad9
Atimscreative <timilehinsunday144@gmail.com>
2024-03-05 19:04:02 +0100

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import KoinX from "../assets/koin-x.svg";
+
+const Header = () => {
+  return (
+    <Header>
+      <div className="container mx-auto px-4">
+        <img src={KoinX} alt="Koin X Logo" />
+      </div>
+    </Header>
+  );
+};
+
+export default Header;
